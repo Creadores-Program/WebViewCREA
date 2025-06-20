3639958f519651b12065c2f4fc5d9cb5cb3e5e26
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2025-06-19 22:36:09 -0600

    Implementar URL de código

--- WebViewCREA/src/main/java/org/CredoresProgram/WebViewCREA/WebViewCreaClient.java
+++ WebViewCREA/src/main/java/org/CredoresProgram/WebViewCREA/WebViewCreaClient.java
@@ -21,30 +21,34 @@ public class WebViewCreaClient extends WebViewClient{
     @Override
     public WebResourceResponse shouldInterceptRequest(WebView view, String url){
         String[] partes = url.split("/");
-        if(url.toLowerCase().endsWith(".html") || !partes[partes.length - 1].contains(".")){
-            Request request = new Request.Builder()
-            .url(url)
-            .addHeader("User-Agent", view.getSettings().getUserAgentString())
-            .build();
-            Response response = null;
+        if(url.toLowerCase().endsWith(".html") || !partes[partes.length - 1].contains(".") || url.toLowerCase().startsWith("data:text/html,")){
             try {
-                response = client.newCall(request).execute();
+              Document content;
+              if(url.toLowerCase().startsWith("data:text/html,")){
+                  content = Jsoup.parse(url.replaceFirst("data:text/html,"));
+              }else{
+                Request request = new Request.Builder()
+                  .url(url)
+                  .addHeader("User-Agent", view.getSettings().getUserAgentString())
+                  .build();
+                Response response = client.newCall(request).execute();
                 if (!response.isSuccessful() || response.body() == null) {
                     return super.shouldInterceptRequest(view, url);
                 }
-                Document content = Jsoup.parse(response.body().string());
-                Element head = content.head();
-                for (Element script : content.select("script")) {
+                content = Jsoup.parse(response.body().string());
+              }
+              Element head = content.head();
+              for (Element script : content.select("script")) {
                     if (script.attr("type").isEmpty() || script.attr("type").equalsIgnoreCase("text/javascript") || script.attr("type").equalsIgnoreCase("application/javascript")) {
                         script.attr("type", "text/babel");
                     }
-                }
-                if (head != null) {
+              }
+              if (head != null) {
                     String urlPrefix = "file:///android_asset/WebViewCrea";
                     head.prepend("<script src='"+urlPrefix+"/babel.min.js'></script>\n<script src='"+urlPrefix+"/runtime.js' type='text/babel'></script>\n<script src='"+urlPrefix+"/core-js.min.js' type='text/babel'></script>\n<script src='"+urlPrefix+"/customEventPoly.js' type='text/babel'></script>\n<script src='"+urlPrefix+"/fetch.js' type='text/babel'></script>\n<script src='"+urlPrefix+"/url-polyfill.js' type='text/babel'></script>\n");
-                }
-                InputStream inputStream = new ByteArrayInputStream(content.html().getBytes("UTF-8"));
-                return new WebResourceResponse("text/html", "UTF-8", inputStream);
+              }
+              InputStream inputStream = new ByteArrayInputStream(content.html().getBytes("UTF-8"));
+              return new WebResourceResponse("text/html", "UTF-8", inputStream);
             }catch(Exception e){
                 return super.shouldInterceptRequest(view, url);
             }
