d6d88447a927e85c033ab64e59e7b79405ac667a
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2026-08-22 17:51:26 -0600

    Enhance API handler for patchHtml with method checks
    Implement POST and OPTIONS method handling with error management.

--- backend/pages/api/patchHtml.js
+++ backend/pages/api/patchHtml.js
@@ -1,1 +1,22 @@
-export default async function handler(req, res) {}
+import { setHeaders } from '../../utils/Utils.js';
+import { patchHtml } from '../../patch/patchHtml.js';
+export default async function handler(req, res) {
+  if(req.method != 'POST' && req.method != 'OPTIONS'){
+    res.status(405).json({ error: 'Method Not Allowed' });
+    return;
+  }
+  setHeaders(res);
+  if(req.method == 'OPTIONS'){
+    res.status(200).end();
+    return;
+  }
+  res.setHeader('Content-Type', 'text/html; charset=utf-8');
+  try{
+    const data = req.body;
+    const codePatch = await patchHtml(data);
+    res.status(200).send(codePatch);
+  }catch(err){
+    console.error(err);
+    res.status(200).send(req.body);
+  }
+}
