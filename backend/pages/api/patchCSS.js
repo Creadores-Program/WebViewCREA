5649dd9be46b7aa9d8f409c4d2768ffa9d94d032
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2026-08-22 17:48:50 -0600

    Implement CSS patching API handler

--- backend/pages/api/patchCSS.js
+++ backend/pages/api/patchCSS.js
@@ -1,1 +1,22 @@
-export default async function handler(req, res) {}
+import { setHeaders } from '../../utils/Utils.js';
+import { patchCss } from '../../patch/patchCss.js';
+export default async function handler(req, res) {
+  if(req.method != 'POST' && req.method != 'OPTIONS'){
+    res.status(405).json({ error: 'Method Not Allowed' });
+    return;
+  }
+  setHeaders(res);
+  if(req.method == 'OPTIONS'){
+    res.status(200).end();
+    return;
+  }
+  res.setHeader('Content-Type', 'text/css; charset=utf-8');
+  try{
+    const data = req.body;
+    const codePatch = await patchCss(data);
+    res.status(200).send(codePatch);
+  }catch(err){
+    console.error(err);
+    res.status(200).send(req.body);
+  }
+}
