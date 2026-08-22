3d0c4471e94df8eb62066079197e08b3d484afe3
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2026-08-22 13:12:50 -0600

    Update autoprefixer to include more browsers
    Added support for additional browsers in autoprefixer configuration.

--- backend/patch/patchCss.js
+++ backend/patch/patchCss.js
@@ -2,4 +2,36 @@ import postcss from 'postcss';
 import postcssPresetEnv from 'postcss-preset-env';
 import autoprefixer from 'autoprefixer';
 
-export default async function patchCss(css){}
+export default async function patchCss(css){
+  return await postcss([
+    postcssPresetEnv({
+      stage: 0,
+      browsers: [
+        "ie >= 8",
+        "firefox >= 3.5",
+        "chrome >= 3",
+        "opera >= 10",
+        "safari >= 4",
+        "android >= 2.1"
+      ],
+      autoprefixer: {
+        grid: 'autoplace'
+      },
+      features: {
+        'pseudo-elements-single-colon': true,
+        'custom-properties': { preserve: false }
+      }
+    }),
+    autoprefixer({
+      overrideBrowserslist: [
+        "ie >= 8",
+        "firefox >= 3.5",
+        "chrome >= 3",
+        "opera >= 10",
+        "safari >= 4",
+        "android >= 2.1"
+      ],
+      grid: 'autoplace'
+    })
+  ]).process(css, { from: undefined });
+}
