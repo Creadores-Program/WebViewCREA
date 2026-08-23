6aed5e409233fcf2309bb7e5f5982e15d532a9df
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2026-08-22 18:45:45 -0600

    Add postcssPseudoelements to CSS processing

--- backend/patch/patchCss.js
+++ backend/patch/patchCss.js
@@ -1,9 +1,13 @@
 import postcss from 'postcss';
 import postcssPresetEnv from 'postcss-preset-env';
+import postcssPseudoelements from 'postcss-pseudoelements';
 import autoprefixer from 'autoprefixer';
 
 export default async function patchCss(css){
   const result = await postcss([
+    postcssPseudoelements({ 
+      single: true
+    }),
     postcssPresetEnv({
       stage: 0,
       browsers: [
@@ -21,18 +25,7 @@ export default async function patchCss(css){
         'pseudo-elements-single-colon': true,
         'custom-properties': { preserve: false }
       }
-    }),
-    autoprefixer({
-      overrideBrowserslist: [
-        "ie >= 8",
-        "firefox >= 3.5",
-        "chrome >= 3",
-        "opera >= 10",
-        "safari >= 4",
-        "android >= 2.1"
-      ],
-      grid: 'autoplace'
-    })
+    })
   ]).process(css, { from: undefined });
   return result.css;
 }
