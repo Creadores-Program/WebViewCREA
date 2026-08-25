69bb02d076293f24d7de5829a336824c365fc4d2
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2026-08-25 10:16:56 -0600

    Add user agent strings for various browsers

--- backend/utils/UserAgent.js
+++ backend/utils/UserAgent.js
@@ -1,1 +1,8 @@
+export const defWebViewCREAMobile = "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/151.0.0.0 Mobile Safari/537.36 WebViewCREA/1.1.0";
+export const firefoxMobile = "Mozilla/5.0 (Android 13; Mobile; rv:154.0) Gecko/154.0 Firefox/154.0";
+export const safariMobile = "Mozilla/5.0 (iPhone; CPU iPhone OS 17_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.5 Mobile/15E148 Safari/605.1.15";
+export const chromeDesktop = "Mozilla/5.0 (X11; Linux; x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/151.0.0.0 Safari/537.36";
+export const defWebViewCREADesktop = "Mozilla/5.0 (X11; Linux; x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/151.0.0.0 Safari/537.36 WebViewCREA/1.1.0";
+export const firefoxDesktop = "Mozilla/5.0 (X11; Linux x86_64; rv:154.0) Gecko/20100101 Firefox/154.0";
+export const safariDesktop = "Mozilla/5.0 (Macintosh; Intel Mac OS X 14_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.5 Safari/605.1.15";
 export default "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/151.0.0.0 Mobile Safari/537.36";
