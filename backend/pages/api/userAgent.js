680ac21a748c234f7b2c2c1fc20a49e4f15d8d2f
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2026-08-25 10:25:17 -0600

    Add userAgent API handler for POST and OPTIONS methods

---
+++ backend/pages/api/userAgent.js
@@ -0,0 +1,21 @@
+import setHeaders from '../../utils/Utils.js';
+import * as userAgents from '../../utils/UserAgent.js';
+
+export default async function handler(req, res) {
+  if(req.method != 'POST' && req.method != 'OPTIONS'){
+    res.status(405).json({ error: 'Method Not Allowed' });
+    return;
+  }
+  setHeaders(res);
+  if(req.method == 'OPTIONS'){
+    res.status(200).end();
+    return;
+  }
+  try{
+    const data = req.body;
+    res.status(200).send(userAgents[data] || userAgents.default);
+  }catch(e){
+    console.error(e);
+    res.status(500).send();
+  }
+}
