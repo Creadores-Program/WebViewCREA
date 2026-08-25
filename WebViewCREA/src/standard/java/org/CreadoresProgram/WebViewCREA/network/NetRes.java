ce4d6ab8043f193a7e6a6124e7dd090d295e6663
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2026-08-25 13:52:53 -0600

    Add Response field to NetRes class

--- WebViewCREA/src/standard/java/org/CreadoresProgram/WebViewCREA/network/NetRes.java
+++ WebViewCREA/src/standard/java/org/CreadoresProgram/WebViewCREA/network/NetRes.java
@@ -1,4 +1,11 @@
 package org.CreadoresProgram.WebViewCREA.network;
+
+import okhttp3.Response;
+
 public class NetRes {
-  
+  private Response response;
+
+  public NetRes(Response response){
+    this.response = response;
+  }
 }
