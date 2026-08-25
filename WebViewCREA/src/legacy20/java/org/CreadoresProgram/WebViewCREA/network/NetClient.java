23561ccfb41f8813abefda9f0b4d384574ce1d95
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2026-08-25 11:01:44 -0600

    Add networking imports to NetClient.java
    Added imports for networking functionality.

--- WebViewCREA/src/legacy20/java/org/CreadoresProgram/WebViewCREA/network/NetClient.java
+++ WebViewCREA/src/legacy20/java/org/CreadoresProgram/WebViewCREA/network/NetClient.java
@@ -1,2 +1,10 @@
 package org.CreadoresProgram.WebViewCREA.network;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class NetClient{}
