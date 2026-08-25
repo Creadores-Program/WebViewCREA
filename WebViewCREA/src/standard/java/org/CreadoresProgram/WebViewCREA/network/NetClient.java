21d4a2e09b49da0983aea45f0f9a7c009aec3d05
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2026-08-25 13:53:11 -0600

    Implement OkHttpClient in NetClient class
    Added OkHttpClient with timeout settings to NetClient class.

--- WebViewCREA/src/standard/java/org/CreadoresProgram/WebViewCREA/network/NetClient.java
+++ WebViewCREA/src/standard/java/org/CreadoresProgram/WebViewCREA/network/NetClient.java
@@ -2,4 +2,10 @@ package org.CreadoresProgram.WebViewCREA.network;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
-public class NetClient{}
+public class NetClient{
+  private OkHttpClient clientHt = new OkHttpClient.Builder()
+    .connectTimeout(60, TimeUnit.SECONDS)
+    .writeTimeout(60, TimeUnit.SECONDS)
+    .readTimeout(60, TimeUnit.SECONDS)
+    .build();
+}
