89178a59d933152a54068cda23fca02847aebc71
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2026-08-29 16:27:13 -0600

    Set connection headers in setHeaders function
    Added headers for connection management.

--- backend/utils/Utils.js
+++ backend/utils/Utils.js
@@ -2,4 +2,6 @@ export default function setHeaders(res){
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Connection', 'keep-alive');
+  req.setHeader('Keep-Alive', 'timeout=60, max=100');
 }
