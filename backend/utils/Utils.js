865430abf1a4de771bb82f09e46cbea57debd88b
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2026-08-29 17:34:25 -0600

    Fix Keep-Alive header setting in Utils.js

--- backend/utils/Utils.js
+++ backend/utils/Utils.js
@@ -3,5 +3,5 @@ export default function setHeaders(res){
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   res.setHeader('Connection', 'keep-alive');
-  req.setHeader('Keep-Alive', 'timeout=60, max=100');
+  res.setHeader('Keep-Alive', 'timeout=60, max=100');
 }
