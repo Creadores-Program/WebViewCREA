0ae77d48b13d3c21134753a55db04fcce8525613
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2026-08-29 16:36:20 -0600

    Add ConnectionPool to OkHttpClient configuration

--- WebViewCREA/src/standard/java/org/CreadoresProgram/WebViewCREA/network/NetClient.java
+++ WebViewCREA/src/standard/java/org/CreadoresProgram/WebViewCREA/network/NetClient.java
@@ -8,6 +8,7 @@ import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 import okhttp3.MediaType;
+import okhttp3.ConnectionPool;
 
 public class NetClient{
   private OkHttpClient clientHt = new OkHttpClient.Builder()
