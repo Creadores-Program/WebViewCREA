ef08b53edd13c403a6c8970db644f971a9fdf624
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2026-08-29 23:53:07 -0600

    Refactor NetRes class for improved readability

--- WebViewCREA/src/standard/java/org/CreadoresProgram/WebViewCREA/network/NetRes.java
+++ WebViewCREA/src/standard/java/org/CreadoresProgram/WebViewCREA/network/NetRes.java
@@ -2,60 +2,44 @@ package org.CreadoresProgram.WebViewCREA.network;
 
 import okhttp3.Response;
 import okhttp3.Headers;
-import okio.BufferedSource;
-import okio.GzipSource;
-import okio.Okio;
-
 import java.io.IOException;
 import java.util.Map;
 import java.util.HashMap;
 
 public class NetRes {
-    private Response response;
+  private Response response;
 
-    public NetRes(Response response) {
-        this.response = response;
+  public NetRes(Response response){
+    this.response = response;
+  }
+
+  public boolean isSuccessful() throws IOException {
+    return response.isSuccessful();
+  }
+
+  public int getStatusCode() throws IOException {
+    return response.code();
+  }
+
+  public Map<String, String> getHeaders(){
+    Headers headers = response.headers();
+    Map<String, String> headersMap = new HashMap<String, String>();
+    for (String name : headers.names()) {
+      headersMap.put(name.toLowerCase(), response.header(name));
     }
+    return headersMap;
+  }
 
-    public boolean isSuccessful() throws IOException {
-        return response.isSuccessful();
+  public String getData() throws IOException {
+    if (response.body() == null) {
+      return "";
     }
+    return response.body().string();
+  }
 
-    public int getStatusCode() throws IOException {
-        return response.code();
+  public void close(){
+    if (response != null) {
+      response.close();
     }
-
-    public Map<String, String> getHeaders() {
-        Headers headers = response.headers();
-        Map<String, String> headersMap = new HashMap<String, String>();
-        for (String name : headers.names()) {
-            headersMap.put(name.toLowerCase(), response.header(name));
-        }
-        return headersMap;
-    }
-
-    public String getData() throws IOException {
-        if (response.body() == null) {
-            return "";
-        }
-
-        String encoding = response.header("Content-Encoding");
-        
-        if (encoding != null && encoding.toLowerCase().contains("gzip")) {
-            try {
-                BufferedSource gzipSource = Okio.buffer(new GzipSource(response.body().source()));
-                return gzipSource.readUtf8();
-            } catch (Exception e) {
-                return response.body().string();
-            }
-        }
-
-        return response.body().string();
-    }
-
-    public void close() {
-        if (response != null) {
-            response.close();
-        }
-    }
+  }
 }
