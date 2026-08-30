d44fcd80ac38616c7d2230084bc9cd2dde5a25c2
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2026-08-30 00:04:17 -0600

    Refactor NetClient to simplify post request handling
    Removed Gzip compression from request body handling in post method.

--- WebViewCREA/src/standard/java/org/CreadoresProgram/WebViewCREA/network/NetClient.java
+++ WebViewCREA/src/standard/java/org/CreadoresProgram/WebViewCREA/network/NetClient.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.io.IOException;
-
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
@@ -15,12 +14,9 @@ import okhttp3.ConnectionPool;
 import okhttp3.Cookie;
 import okhttp3.CookieJar;
 import okhttp3.HttpUrl;
-import okio.BufferedSink;
-import okio.GzipSink;
-import okio.Okio;
 import android.webkit.CookieManager;
 
-public class NetClient {
+public class NetClient{
   private OkHttpClient clientHt = new OkHttpClient.Builder()
     .connectionPool(new ConnectionPool(5, 5, TimeUnit.MINUTES))
     .connectTimeout(60, TimeUnit.SECONDS)
@@ -56,40 +52,16 @@ public class NetClient {
   private static final String lang = Locale.getDefault().getLanguage();
   private MediaType mediaType = MediaType.parse("text/html; charset=utf-8");
 
-  private RequestBody forceGzipRequestBody(final RequestBody body) {
-    return new RequestBody() {
-      @Override
-      public MediaType contentType() {
-        return body.contentType();
-      }
-
-      @Override
-      public long contentLength() {
-        return -1;
-      }
-
-      @Override
-      public void writeTo(BufferedSink sink) throws IOException {
-        BufferedSink gzipSink = Okio.buffer(new GzipSink(sink));
-        body.writeTo(gzipSink);
-        gzipSink.close();
-      }
-    };
-  }
-
-  public NetRes post(String url, String userAgent, boolean isDesktop, String data, String cookie) throws IOException {
-    RequestBody rawBody = RequestBody.create(mediaType, data);
-    RequestBody gzippedBody = forceGzipRequestBody(rawBody);
-
+  public NetRes post(String url, String userAgent, boolean isDesktop, String data, String cookie) throws IOException{
+    RequestBody body = RequestBody.create(mediaType, data);
     Request req = new Request.Builder()
       .url(url)
-      .post(gzippedBody)
-      .header("Content-Encoding", "gzip")
+      .post(body)
       .header("Accept-Language", lang)
       .header("User-Agent", userAgent)
       .header("Sec-CH-UA", "\"WebViewCREA\";v=\"1\", \"Chromium\";v=\"125\", \"Not.A/Brand\";v=\"24\"")
       .header("Sec-CH-UA-Mobile", "?" + (isDesktop ? "0" : "1"))
-      .header("Sec-CH-UA-Platform", "\"" + (isDesktop ? "Linux" : "Android") + "\"")
+      .header("Sec-CH-UA-Platform", "\""+(isDesktop ? "Linux" : "Android")+"\"")
       .header("Upgrade-Insecure-Requests", "1")
       .header("Connection", "keep-alive")
       .header("Keep-Alive", "timeout=60, max=100")
@@ -99,19 +71,18 @@ public class NetClient {
       .header("Sec-Fetch-User", "?1")
       .header("Cookie", (cookie != null) ? cookie : "")
       .build();
-
     Response res = clientHt.newCall(req).execute();
     return new NetRes(res);
   }
 
-  public NetRes get(String url, String userAgent, boolean isDesktop, String cookie) throws IOException {
+  public NetRes get(String url, String userAgent, boolean isDesktop, String cookie) throws IOException{
     Request req = new Request.Builder()
       .url(url)
       .header("Accept-Language", lang)
       .header("User-Agent", userAgent)
       .header("Sec-CH-UA", "\"WebViewCREA\";v=\"1\", \"Chromium\";v=\"151\", \"Not.A/Brand\";v=\"24\"")
       .header("Sec-CH-UA-Mobile", "?" + (isDesktop ? "0" : "1"))
-      .header("Sec-CH-UA-Platform", "\"" + (isDesktop ? "Linux" : "Android") + "\"")
+      .header("Sec-CH-UA-Platform", "\""+(isDesktop ? "Linux" : "Android")+"\"")
       .header("Upgrade-Insecure-Requests", "1")
       .header("Connection", "keep-alive")
       .header("Keep-Alive", "timeout=60, max=100")
@@ -120,16 +91,15 @@ public class NetClient {
       .header("Sec-Fetch-Site", "cross-site")
       .header("Sec-Fetch-User", "?1")
       .build();
-
     Response res = clientHt.newCall(req).execute();
     return new NetRes(res);
   }
 
-  public OkHttpClient getOkClient() {
+  public OkHttpClient getOkClient(){
     return this.clientHt;
   }
 
-  public void setOkClient(OkHttpClient clientHt) {
+  public void setOkClient(OkHttpClient clientHt){
     this.clientHt = clientHt;
   }
 }
