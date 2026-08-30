2312e9c2a47b03fe9da3bacb4b26ca9ae9ac5f4a
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2026-08-30 09:38:01 -0600

    Add Cache-Control and Vary headers to response

--- backend/utils/Utils.js
+++ backend/utils/Utils.js
@@ -4,4 +4,6 @@ export default function setHeaders(res){
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   res.setHeader('Connection', 'keep-alive');
   res.setHeader('Keep-Alive', 'timeout=60, max=100');
+  res.setHeader('Cache-Control', 'public, max-age=864000, s-maxage=864000, stale-while-revalidate=86400');
+  res.setHeader('Vary', 'target-url, Accept, Content-Type');
 }
