ed77f53054278e752f3733be76369ab136ed4c2f
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2026-08-30 10:57:02 -0600

    Delete backend/utils/Utils.js

--- backend/utils/Utils.js
+++
@@ -1,9 +0,0 @@
-export default function setHeaders(res){
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  res.setHeader('Connection', 'keep-alive');
-  res.setHeader('Keep-Alive', 'timeout=60, max=100');
-  res.setHeader('Cache-Control', 'public, max-age=864000, s-maxage=864000, stale-while-revalidate=86400');
-  res.setHeader('Vary', 'target-url, Accept, Content-Type');
-}
