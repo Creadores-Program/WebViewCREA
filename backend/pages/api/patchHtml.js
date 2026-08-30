f3dc580e3b79386ef067fc500da9802791ed8a1e
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2026-08-30 09:26:42 -0600

    Set body parser size limit to 4mb
    Add body parser size limit for API requests

--- backend/pages/api/patchHtml.js
+++ backend/pages/api/patchHtml.js
@@ -1,5 +1,12 @@
 import setHeaders from '../../utils/Utils.js';
 import patchHtml from '../../patch/patchHtml.js';
+export const config = {
+  api: {
+    bodyParser: {
+      sizeLimit: '4mb',
+    },
+  },
+};
 export default async function handler(req, res) {
   if(req.method != 'POST' && req.method != 'OPTIONS'){
     res.status(405).json({ error: 'Method Not Allowed' });
