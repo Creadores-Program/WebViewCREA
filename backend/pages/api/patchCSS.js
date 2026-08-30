eb223c4591bca8468e7365b37f561ae314b7b873
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2026-08-30 10:57:28 -0600

    Remove unused setHeaders import from patchCSS.js
    Removed unused import of setHeaders function.

--- backend/pages/api/patchCSS.js
+++ backend/pages/api/patchCSS.js
@@ -1,4 +1,3 @@
-import setHeaders from '../../utils/Utils.js';
 import patchCss from '../../patch/patchCss.js';
 export const config = {
   api: {
@@ -12,7 +11,6 @@ export default async function handler(req, res) {
     res.status(405).json({ error: 'Method Not Allowed' });
     return;
   }
-  setHeaders(res);
   if(req.method == 'OPTIONS'){
     res.status(200).end();
     return;
